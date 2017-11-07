1f427d993bf27d615b0970a2d9795c1e72d92fb3
Venkata Prakash Ankireddy <venkataprakash.ankireddy@honeywell.com>
2017-11-07 07:51:36 +0530

    routing with parameters completed

--- src/AngularGettingStarted/src/app/products/product-detail.component.ts
+++ src/AngularGettingStarted/src/app/products/product-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Iproduct } from './product';
 
 @Component({
@@ -9,9 +10,21 @@ export class ProductDetailComponent implements OnInit {
     pageTitle: string = 'Product Detail';
     product: Iproduct;
 
-  constructor() { }
+    constructor(private _route: ActivatedRoute) { }
 
-  ngOnInit() {
+    ngOnInit() {
+        let id = +this._route.snapshot.paramMap.get('id')
+        this.pageTitle += `: ${id}`;
+        this.product = {
+            "productId": id,
+            "productName": "Leaf Rake",
+            "productCode": "GDN-0011",
+            "releaseDate": "March 19, 2016",
+            "description": "Leaf rake with 48-inch wooden handle.",
+            "price": 19.95,
+            "starRating": 3.2,
+            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+        }
   }
 
 }
